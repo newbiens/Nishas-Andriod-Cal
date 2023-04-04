60a09f6d8146c42704e1ddae719188e941a51c77
Nisha Srivastava <67898495+newbiens@users.noreply.github.com>
2023-04-04 12:39:30 +0530

    Add files via upload

---
+++ generated/source/buildConfig/debug/com/DataFlair/mycalculator/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.DataFlair.mycalculator;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "net.pubnative.hybidstandalonedemo";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 20407;
+  public static final String VERSION_NAME = "2.4.7";
+}
